1382b7e9424ac8f72d8d9dcc61a168619e9845ab
Lucas Burson <ljdelight@gmail.com>
2023-04-15 13:04:42 -0500

    Use GitHub Actions to sign and publish the artifacts

--- .travis/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -x
-
-if [ "$TRAVIS_BRANCH" = "main" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
-then
-    openssl aes-256-cbc -K $encrypted_6afd0fc9428e_key -iv $encrypted_6afd0fc9428e_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
-fi
